570236fed1a3cc10b58f3cfd41752406506ae9ed
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-28 12:41:43 +0100

    create web socet  and write value in Firebase

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { database } from "./firebaseConfig"
 
 class Counter extends React.Component {
     state = {
         number: 0
     }
 
-    incHandler = () => this.setState({ number: this.state.number + 1 })
-    decHandler = () => this.setState({ number: this.state.number - 1 })
+    componentDidMount() {
+        database.ref('/counter').on(
+            'value',
+            snapshot => this.setState({ number: snapshot.val() })
+        )
+    }
 
+
+
+    incHandler = () => database.ref('/counter').set(  this.state.number + 1 )
+    decHandler = () =>  database.ref('/counter').set( this.state.number - 1 )
 
 
 
