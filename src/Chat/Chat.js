b60df5e11ea7eb22973b41dca9461c86ff4d7387
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-28 15:02:55 +0100

    Syncing on messages

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,35 +1,67 @@
-import React from 'react'
+mport React from 'react'
+
 import { database } from '../firebaseConfig'
 
 import NewMessageForm from './NewMessageForm'
 
+const dbMessagesRef = database.ref('/jfddl6-messages')
+
 class Chat extends React.Component {
-    state = {
-        newMessageText: 'krowa'
-    }
+  state = {
+    newMessageText: 'krowa',
+    messages: []
+  }
 
-    onNewMessageTextChangeHandler = event => (
-        this.setState({ newMessageText: event.target.value })
+  componentDidMount() {
+    dbMessagesRef.on(
+      'value',
+      snapshot => {
+        const messages = Object.entries(
+          snapshot.val()
+        ).map(entry => ({
+          ...entry[1],
+          key: entry[0]
+        }))
+
+        this.setState({ messages: messages })
+      }
     )
+  }
 
-    onNewMessageAddClickHandler = () => {
-        database.ref('/Chat').push({
-            text: this.state.newMessageText,
-            timestamp: Date.now()
-        })
-        this.setState({ newMessageText: '' })
-    }
+  componentWillUnmount(){
+    dbMessagesRef.off()
+  }
 
-    render() {
-        return (
-            <div>
-                <NewMessageForm
-                    newMessageText={this.state.newMessageText}
-                    onNewMessageTextChangeHandler={this.onNewMessageTextChangeHandler}
-                    onNewMessageAddClickHandler={this.onNewMessageAddClickHandler}
-                />
+  onNewMessageTextChangeHandler = event => (
+    this.setState({ newMessageText: event.target.value })
+  )
+
+  onNewMessageAddClickHandler = () => {
+    dbMessagesRef.push({
+      text: this.state.newMessageText,
+      timestamp: Date.now()
+    })
+    this.setState({ newMessageText: '' })
+  }
+
+  render() {
+    return (
+      <div>
+        <NewMessageForm
+          newMessageText={this.state.newMessageText}
+          onNewMessageTextChangeHandler={this.onNewMessageTextChangeHandler}
+          onNewMessageAddClickHandler={this.onNewMessageAddClickHandler}
+        />
+        {
+          this.state.messages.map(message => (
+            <div
+              key={message.key}
+            >
+              {message.text}
             </div>
-        )
-    }
+          ))
+        }
+      </div>
+    )
+  }
 }
-export default Chat
