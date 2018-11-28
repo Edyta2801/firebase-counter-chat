e58c2084a21f8353fcfa0db9d4c1371b6ef76883
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-28 14:37:14 +0100

    send message to Firebase

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,28 +1,35 @@
 import React from 'react'
+import { database } from '../firebaseConfig'
 
 import NewMessageForm from './NewMessageForm'
 
 class Chat extends React.Component {
-  state = {
-    newMessageText: 'krowa'
-  }
+    state = {
+        newMessageText: 'krowa'
+    }
 
-  onNewMessageTextChangeHandler = event => (
-    this.setState({ newMessageText: event.target.value })
-  )
-
-  onNewMessageAddClickHandler = () => {}
-
-  render() {
-    return (
-      <div>
-        <NewMessageForm 
-          newMessageText={this.state.newMessageText}
-          onNewMessageTextChangeHandler={this.onNewMessageTextChangeHandler}
-          onNewMessageAddClickHandler={this.onNewMessageAddClickHandler}
-        />
-      </div>
+    onNewMessageTextChangeHandler = event => (
+        this.setState({ newMessageText: event.target.value })
     )
-  }
+
+    onNewMessageAddClickHandler = () => {
+        database.ref('/Chat').push({
+            text: this.state.newMessageText,
+            timestamp: Date.now()
+        })
+        this.setState({ newMessageText: '' })
+    }
+
+    render() {
+        return (
+            <div>
+                <NewMessageForm
+                    newMessageText={this.state.newMessageText}
+                    onNewMessageTextChangeHandler={this.onNewMessageTextChangeHandler}
+                    onNewMessageAddClickHandler={this.onNewMessageAddClickHandler}
+                />
+            </div>
+        )
+    }
 }
 export default Chat
